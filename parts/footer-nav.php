71d04fa709f743d8a648fac82755248665c19888
David Mills <david@unisonagency.com>
2014-09-09 10:01:59 -0400

    fixed copyright

--- parts/footer-nav.php
+++ parts/footer-nav.php
@@ -1,4 +1,4 @@
 <div class="copyright">
-	&copy; <?php echo date("Y") ?> B2Buy. All rights reserved.
+	&copy; <?php echo date("Y") ?> FedBid. All rights reserved.
 </div> 
 <?php wp_nav_menu( array( 'theme_location' => 'footer-menu') ); ?>
