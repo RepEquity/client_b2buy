bd74ca50e763c258b79e1530970175d2dc15a5cd
Tedi Konda <tedikonda@gmail.com>
2014-09-10 08:54:42 -0400

    removed rights reserved and re-centered.

--- parts/footer-nav.php
+++ parts/footer-nav.php
@@ -1,4 +1,4 @@
 <div class="copyright">
-	&copy; <?php echo date("Y") ?> FedBid. All rights reserved.
+	&copy; <?php echo date("Y") ?> FedBid.
 </div> 
 <?php wp_nav_menu( array( 'theme_location' => 'footer-menu') ); ?>
