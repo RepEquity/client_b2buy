cb0ff82cb620d468c88f0a6e0d999f6faee9808d
Tedi Konda <tedikonda@gmail.com>
2014-09-11 02:08:28 -0400

    Final tickets style fixes.

--- parts/footer-nav.php
+++ parts/footer-nav.php
@@ -1,4 +1,4 @@
 <div class="copyright">
-	&copy; <?php echo date("Y") ?> FedBid.
+	&copy; <?php echo date("Y") ?> FedBid, Inc.
 </div> 
 <?php wp_nav_menu( array( 'theme_location' => 'footer-menu') ); ?>
