eaafbad18be74e1dd681394e07838a517825ee92
David Mills <david@unisonagency.com>
2014-09-22 13:17:24 -0400

    added clicky

--- footer.php
+++ footer.php
@@ -33,6 +33,11 @@
 	    })(document,"script","hs-analytics",300000);
 	  </script>
 	<!-- End of Async HubSpot Analytics Code -->
+	
+	<script src="//static.getclicky.com/js" type="text/javascript"></script>
+	<script type="text/javascript">try{ clicky.init(100773847); }catch(e){}</script>
+	<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100773847ns.gif" /></p></noscript>
+	
 <?php endif; ?>
 
 </body>
